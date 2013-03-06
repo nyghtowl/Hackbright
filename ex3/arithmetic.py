5ba91f89e66908b10ac1b0aca8b4bf356d4b2251
Hackbright student <user@mini-005.(none)>
2013-03-06 15:41:01 -0800

    Update with excercise 3

--- ex3/arithmetic.py
+++ ex3/arithmetic.py
@@ -1,31 +1,23 @@
-def add(first, second):
-    print "DEBUG MESSAGE: this is a dummy add function"
-    return 0
+def add(num1, num2):
+    return num1 + num2
 
-def subtract(first, second):
-    print "DEBUG MESSAGE: this is a dummy subtract function"
-    return 0
+def subtract(num1, num2):
+    return num1 - num2
 
-def multiply(first, second):
-    print "DEBUG MESSAGE: this is a dummy multiply function"
-    return 0
+def multiply(num1, num2):
+    return num1 * num2
 
-def divide(first, second):
-    print "DEBUG MESSAGE: this is a dummy divide function"
-    return 0.0
+def divide(num1, num2):
+    return num1 / num2
 
-def square(i):
-    print "DEBUG MESSAGE: this is a dummy square function"
-    return 0
+def square(num1):
+    return num1 * num1 
 
-def cube(i):
-    print "DEBUG MESSAGE: this is a dummy cube function"
-    return 0
+def cube(num1):
+    return num1**3
 
-def mod(i, j):
-    print "DEBUG MESSAGE: this is a dummy mod function"
-    return 0
+def power(num1, num2):
+    return num1**num2
 
-def power(i, j):
-    print "DEBUG MESSAGE: this is a dummy power function"
-    return 0
+def mod(num1, num2):
+    return num1 % num2
