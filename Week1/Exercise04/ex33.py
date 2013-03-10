801a51affea72a2ecbeb10a42f0efd4cc67e6a43
nyghtowl <mwarrickhb@gmail.com>
2013-03-09 20:06:03 -0800

    Hackbright Wk1 Exercises

---
+++ Week1/Exercise04/ex33.py
@@ -0,0 +1,43 @@
+import random
+
+def topBot(x, step):
+	i = 0
+	numbers = []
+
+	for i in range(x):
+		print "At the top i is %d" % i
+		numbers.append(i)
+
+#		i += step - this is just ignored and the one in range steps through the whole range
+		print "Numbers now: ", numbers
+		print "At the bottom i is %d" % i
+
+	print "The numbers: "
+
+	for num in numbers:
+		print num
+
+y = random.randint(0,20)
+topBot(5, 2)
+
+'''
+def topBot(x, step):
+	i = 0
+	numbers = []
+
+	while i < x:
+		print "At the top i is %d" % i
+		numbers.append(i)
+
+		i += step
+		print "Numbers now: ", numbers
+		print "At the bottom i is %d" % i
+
+	print "The numbers: "
+
+	for num in numbers:
+		print num
+
+y = random.randint(0,20)
+topBot(y, 2)
+'''
